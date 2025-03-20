4ec12bbe2027cf487f0f255f542675f0d7dc7e08
Marcos Gabriel Dias Fernandes <110645752+GambollGabriel@users.noreply.github.com>
2025-03-20 01:49:10 +0000

    Base de um código, usa o Slmim. Aprendemos sobre MVC também.

--- app/index.php
+++ app/index.php
@@ -1,8 +1,49 @@
 <?php
-// require './vendor/autoload.php';
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
 
-// use Application\DBConnection\MySQLConnection;
+use Slim\Views\PhpRenderer;
 
-// $db = new MySQLConnection();
+require __DIR__ . '/vendor/autoload.php';
 
-echo phpinfo();
+$app = AppFactory::create();
+
+$app->get(
+    '/',
+    function(
+        Request $request,
+        Response $response,
+        $args
+    ){
+        $response->getBody()->write("Olá mundo!");
+        return $response;
+});
+
+$app->get(
+    '/mensagem/{nome}',
+    function(
+        Request $request,
+        Response $response,
+        $args
+    ){
+        $response->getBody()->write("Boa noite!");
+        return $response;
+});
+
+$app->get(
+    '/hello',
+    function(
+        Request $request,
+        Response $response,
+        $args
+    ){
+        $renderer = new PhpRenderer(__DIR__ . '/templates');
+        $msg = 'Nova mensagem';
+        return $renderer->render($response, 'hello.php', ['mensagem' => $msg]);
+});
+
+
+$app->run();
+
+//Tipo um condominio como uma portaria, sempre vai passar pelo 
